9e037fae9cb0d680a453ffac2d41a0cbe28b38be
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-02-26 17:25:23 -0600

    add nav and about page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div>
+    <nav>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+      </ul>
+    </nav>
+  <Component {...pageProps} />
+ </div>
+  )
 }
